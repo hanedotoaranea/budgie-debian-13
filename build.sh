552c7fdfcdd1b52bdb9dbb94eaa09e49593704d4
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-07 17:09:24 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
     lb config -d trixie \
           --debian-installer live \
+          --architectures amd64 \
           --debian-installer-distribution trixie \
           --debian-installer-gui true \
           --archive-areas "main contrib non-free" \
