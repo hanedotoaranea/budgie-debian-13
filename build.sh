7213dfd502aa638bd0cf945aeb7e127a05ef4938
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-07 17:35:21 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
     lb config -d trixie \
           --debian-installer live \
-           --iso-volume "Alice" \
+          --iso-volume "Alice" \
           --architectures amd64 \
+          --binary-images iso \
           --debian-installer-distribution trixie \
           --debian-installer-gui true \
           --archive-areas "main contrib non-free" \
           --debootstrap-options "--variant=standard" \
           --iso-publisher "hanedotoaranea"
           --checksums sha256 \
+          --mirror-bootstrap http://deb.debian.org/debian \
+          --mirror-chroot http://deb.debian.org/debian \
+          --mirror-chroot-security http://security.debian.org/debian-security \
+          --mirror-binary http://deb.debian.org/debian \
+          --mirror-binary-security http://security.debian.org/debian-security
     
 sudo lb build
